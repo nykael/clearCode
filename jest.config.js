39df1470712ad274c3dcc74e07b0386f3caedadd
nykael <nykaelccardial@gmail.com>
2025-03-18 22:54:32 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  testEnvironment: "jsdom", // Define o ambiente de testes como um navegador simulado
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: "node",
   transform: {
-    "^.+\\.(ts|tsx)$": "babel-jest", // Usa o Babel para transformar os arquivos JS/TS
+    "^.+\\.(js|jsx|ts|tsx)$": "ts-jest",
   },
-  moduleFileExtensions: ["ts", "tsx", "json"], // Extensões de arquivos suportadas
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"], // Arquivo de configuração adicional após o ambiente de testes ser configurado
-  collectCoverage: true, // Ativa a coleta de cobertura de testes
-  collectCoverageFrom: ["src/**/*.{ts,tsx}"], // Define os arquivos para cobertura
-  coverageDirectory: "coverage", // Diretório onde os relatórios de cobertura serão salvos
-  moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "identity-obj-proxy", // Mapeia arquivos de estilo para evitar erros
-    "^@/(.*)$": "<rootDir>/src/$1", // Atalho para importações dentro da pasta src
-  },
-  testMatch: ["**/__tests__/**/*.test.(js|jsx|ts|tsx)"], // Define o padrão de nomes de arquivos de teste
 };
